e580de06b14077be9d5282d918f26a824e15a555
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-10-18 00:39:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+function App() {
+
+  return (
+    <section>
+      <h1>Build The Community Your Fans Will Love</h1>
+
+      <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+        Create connections with your users as you engage in genuine discussion.</p>
+
+      <button>Register</button>
+
+      <footer>
+        <p>
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>.
+          Coded by <a href="#">Your Name Here</a>.
+        </p>
+      </footer>
+    </section>
+  )
+}
+
+export default App
