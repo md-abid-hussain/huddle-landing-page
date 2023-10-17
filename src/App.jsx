bd0cf28438f6a19d4162f67a5421d7eb0639ca01
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-10-18 01:37:45 +0530

    created content and added styles for mobile viewport

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
+import Logo from '/images/logo.svg'
+import Mockup from '/images/illustration-mockups.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
 
   return (
-    <section>
-      <h1>Build The Community Your Fans Will Love</h1>
+    <section className="bg-custom_violet bg-bg-mobile min-h-screen bg-cover md:bg-bg-desktop text-white p-9">
+      <header className=' mb-14'>
+        <img src={Logo} alt="logo" className=' h-8' />
+      </header>
+      <figure className=' mb-16'>
+        <img src={Mockup} alt="mockup illustration" />
+      </figure>
+      <h1 className=" font-[600] text-[24px] text-center mb-6 ">Build The Community Your Fans Will Love</h1>
 
-      <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+      <p className=' text-base font-[400] text-center mb-6 '>Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
         Create connections with your users as you engage in genuine discussion.</p>
 
-      <button>Register</button>
+      <button className=' block mx-auto bg-white py-[14px] px-16 text-custom_violet text-xs rounded-full mb-14 '>Register</button>
 
-      <footer>
-        <p>
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>.
-          Coded by <a href="#">Your Name Here</a>.
-        </p>
-      </footer>
+      <figure className='flex gap-4 justify-center mb-1'>
+        <FontAwesomeIcon icon={faFacebookF} className='border-[1px] p-[0.25rem] rounded-full h-[20px] w-[20px]' />
+        <FontAwesomeIcon icon={faXTwitter} className='border-[1px] p-[0.25rem] rounded-full h-[20px] w-[20px]' />
+        <FontAwesomeIcon icon={faInstagram} className='border-[1px] p-[0.25rem] rounded-full h-[20px] w-[20px]' />
+      </figure>
     </section>
   )
 }
