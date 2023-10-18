570e13f40230a2bb0f4bef5b14639ff837c55650
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-10-18 16:57:32 +0530

    fixed style for image and footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,16 @@ function App() {
     "border-[2px] p-[0.5rem] rounded-full h-4 w-4 hover:text-custom_magenta hover:border-custom_magenta hover:cursor-pointer sm:w-[22px] sm:h-[22px] mx-1";
 
   return (
-    <section className="bg-custom_violet bg-bg-mobile min-h-screen bg-cover md:bg-bg-desktop text-white p-[44px] lg:px-20 lg:pt-16">
-      <header className=" mb-14">
-        <img src={Logo} alt="logo" className="h-8 sm:h-[44px]" />
-      </header>
+    <section className="bg-custom_violet bg-bg-mobile min-h-screen bg-cover md:bg-bg-desktop text-white px-11 pt-11 pb-9 lg:px-20 lg:pt-16">
+      <section className=" mb-14">
+        <img src={Logo} alt="logo" className="h-8 md:h-[52px]" />
+      </section>
       <div className="lg:flex lg:items-center gap-14">
-        <figure className=" mb-16 lg:flex-1">
+        <figure className=" mb-16 lg:m-w-[720px]">
           <img src={Mockup} alt="mockup illustration" />
         </figure>
         <div className="lg:flex-1">
-          <h1 className=" font-[600] text-[24px] text-center mb-6 sm:text-5xl lg:text-left">
+          <h1 className=" font-[600] text-[24px] text-center mb-6 sm:text-4xl lg:text-left">
             Build The Community Your Fans Will Love
           </h1>
 
@@ -29,12 +29,12 @@ function App() {
             engage in genuine discussion.
           </p>
 
-          <button className=" block mx-auto bg-white py-[14px] px-16 text-custom_violet text-sm rounded-full mb-14 hover:bg-custom_magenta hover:text-white hover:cursor-pointer shadow-md shadow-gray-900 sm:text-xl">
+          <button className=" block mx-auto bg-white py-[14px] px-16 text-custom_violet text-sm rounded-full mb-14 hover:bg-custom_magenta hover:text-white hover:cursor-pointer shadow-md shadow-violet-950 sm:text-xl lg:ml-0">
             Register
           </button>
         </div>
       </div>
-      <figure className="mb-1">
+      <figure>
         <div className="text-center lg:text-right">
           <FontAwesomeIcon icon={faFacebookF} className={socialButtonStyle} />
           <FontAwesomeIcon icon={faXTwitter} className={socialButtonStyle} />
